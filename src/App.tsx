1a193e8065182e2d27866e541cb87f1447dce81f
Koussai <koussaikareem@gmail.com>
2024-09-15 17:26:43 +0300

    build the color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,12 @@ const App = () => {
       </GridItem>
 
       <Show above='lg'>
-        <GridItem area={'aside'} bg={'gold'}>
+        <GridItem area={'aside'}>
           Aside
         </GridItem>
       </Show>
 
-      <GridItem area={'main'} bg={'dodgerblue'}>
+      <GridItem area={'main'}>
         Main
       </GridItem>
     </Grid>
