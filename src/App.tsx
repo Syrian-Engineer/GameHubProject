b7c66d60b871b4f0e593ff722420e1848eee546d
Koussai <koussaikareem@gmail.com>
2024-09-15 12:45:42 +0300

    build a resposive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import {  Grid, GridItem, Show } from '@chakra-ui/react'
 
 const App = () => {
   return (
-    <Button colorScheme='blue'>Button</Button>
+    <>
+    <Grid templateAreas={{
+      base :`"nav" "main"`,
+      lg :`"nav nav" "aside main"` // 1024 px
+    }}>
+      <GridItem area={'nav'} bg={'coral'}>Nav</GridItem>
+
+      <Show above='lg'>
+        <GridItem area={'aside'} bg={'gold'}>Aside</GridItem>
+      </Show>
+      
+      <GridItem area={'main'} bg={'dodgerblue'}>Main</GridItem>
+    </Grid>
+    </>
   )
 }
 
