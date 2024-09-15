0ade9f27ab2a6d52d79b17e98b6062e55365455b
Koussai <koussaikareem@gmail.com>
2024-09-15 17:13:40 +0300

    implement datk mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import App from './App.tsx'
+import theme from './theme.ts'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </StrictMode>,
